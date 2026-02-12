b9cfe27bb5bf4ad5df8e1b84ce63117a55c0829a
Pratiksha S. Salve <pratikshasalve0755@gmail>
2026-02-12 14:00:02 +0530

    student_record_manager.py

---
+++ student_record_manager.py
@@ -0,0 +1,33 @@
+# Mini Project : Student Record Manager
+
+print("\nMini Project : Student Record Manager")
+print("\n")
+name = input("Enter Name: ")
+age = int(input("Enter age: "))
+marks = int(input("Enter marks: "))
+
+student = {
+    "name": name,
+    "age": age,
+    "marks": marks
+}
+
+# Grade Calculation
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 50:
+    grade = "C"
+else:
+    grade = "Fail"
+
+# Add grade to dictionary
+student["grade"] = grade
+
+# Display Student Record
+print("\nStudent Record:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Marks:", student["marks"])
+print("Grade:", student["grade"])
