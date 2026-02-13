d5bed3d10be5e81210ae7871561332408737be93
Pratiksha S. Salve <pratikshasalve0755@gmail>
2026-02-13 23:15:40 +0530

    number_analyzer_pro.py

---
+++ number_analyzer_pro.py
@@ -0,0 +1,35 @@
+#MinProject: Number Analyzer Pro
+
+numbers = []21
+total = 0
+even_count = 0
+odd_count = 0
+for i in range(10):
+    num = int(input("Enter number: "))
+    numbers.append(num)
+
+    total += num
+    if i == 0:
+        highest = num
+        lowest = num
+    else:
+        if num > highest:
+            highest = num
+        if num < lowest:
+            lowest = num
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+# Calculate average after loop
+average = total / len(numbers)
+
+print("\n--- Number Analyzer Pro Result ---")
+print("Numbers:", numbers)
+print("Total:", total)
+print("Average:", average)
+print("Highest:", highest)
+print("Lowest:", lowest)
+print("Even Count:", even_count)
+print("Odd Count:", odd_count)
