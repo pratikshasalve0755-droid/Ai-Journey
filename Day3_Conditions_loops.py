6045ea71c0d5184d524dd11aecc86f44297c96a8
Pratiksha S. Salve <pratikshasalve0755@gmail>
2026-02-17 19:40:15 +0530

    Day3_Conditions_loops.py

--- Day3_Conditions_loops.py
+++ Day3_Conditions_loops.py
@@ -57,20 +57,4 @@ else:
 
 -----------------------------------------------------------------------------------------------------------
 
-# Number Guessing Game
 
-import random
-comp_choice = random.randint(1, 10)      # it generates the nuber between the 1  and 50
-
-while True:
-
-    user_choice = int(input("Enter the Number:"))
-    if comp_choice > user_choice:
-        print(" too low!")
-    elif comp_choice < user_choice:
-        print(" too high!")
-    elif comp_choice == user_choice:
-        print(" Right, you guess the number ")
-        break
-else:
-    print(" invalid input")
